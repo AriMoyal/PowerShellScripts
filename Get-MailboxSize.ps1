1d26e674abc352cc53205775bd03cec6c4b59ffe
Ari <6078108+AriMoyal@users.noreply.github.com>
2021-07-27 10:30:17 +0200

    Update Get-MailboxSize.ps1

--- Get-MailboxSize.ps1
+++ Get-MailboxSize.ps1
@@ -1,1 +1,8 @@
 Get-MailboxStatistics | where {$_.ObjectClass –eq “Mailbox”} | Sort-Object TotalItemSize –Descending | ft @{label=”User”;expression={$_.DisplayName}},@{label=”Total Size (GB)”;expression={$_.TotalItemSize.Value.ToGB()}},@{label=”Items”;expression={$_.ItemCount}},@{label=”Storage Limit”;expression={$_.StorageLimitStatus}} -auto
+
+
+//Delete mailbox by date
+search-mailbox -identity user@contoso -searchquery {received:01/01/2015..01/01/2017} -deletecontent
+
+//to allow from normal shell use
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn;
